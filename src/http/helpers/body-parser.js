a9abb873eda012c8430f188692046e43615b6b94
Martin Hicks <hello@martinhicks.net>
2021-04-18 14:12:46 +0100

    shuld be TextDecoder

--- src/http/helpers/body-parser.js
+++ src/http/helpers/body-parser.js
@@ -1,5 +1,5 @@
 import qs from 'https://deno.land/std@0.93.0/node/querystring.ts'
-const encoder = new TextEncoder();
+const decoder = new TextDecoder();
 /**
  * Arc 6 bodies are always base64 encoded strings with req.isBase64Encoded = true (or null, which we interpolate into `{}`)
  * <Arc 6 bodies are always objects
@@ -39,7 +39,7 @@ export default function parseBody (req) {
     }
 
     if (isFormURLEncoded) {
-      let data = encoder.encode(request.body)
+      let data = decoder.decode(request.body)
       request.body = qs.parse(data)
     }
 
