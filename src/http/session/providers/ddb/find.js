820ef42a0d43ac72a45d859038e6bc7f9ca67252
Martin Hicks <hello@martinhicks.net>
2021-04-21 18:23:51 +0100

    marshall / unmarshall ddb;

--- src/http/session/providers/ddb/find.js
+++ src/http/session/providers/ddb/find.js
@@ -1,19 +1,20 @@
 import dynamo from '../../../../tables/dynamo.js'
 import create from './create.js'
+import { marshall, unmarshall } from 'https://deno.land/x/aws_sdk@v3.13.0.0/util-dynamodb/mod.ts'
 
 export default function _find (name, _idx, callback) {
   dynamo.session(function _gotDB (err, db) {
     if (err) callback(err)
     else {
-      db.GetItemCommand({
+      db.getItem({
         TableName: name,
         ConsistentRead: true,
-        Key: { _idx }
+        Key: marshall({ _idx })
       },
       function _get (err, data) {
         if (err) callback(err)
         else {
-          let result = typeof data === 'undefined' ? false : data.Item
+          let result = typeof data === 'undefined' ? false : unmarshall(data.Item)
           if (result && result._secret) {
             callback(null, result)
           }
