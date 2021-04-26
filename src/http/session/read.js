a428c7b79771f9c73038a6b2d4d7ae941da55263
Martin Hicks <hello@martinhicks.net>
2021-04-26 18:15:07 +0100

    need to read Dneo.env witihn the function for Unit tests...

--- src/http/session/read.js
+++ src/http/session/read.js
@@ -1,10 +1,9 @@
 import jwe from './providers/jwe.js'
 import ddb from './providers/ddb/index.js'
 
-const env = Deno.env.toObject()
-
 export default function read (request, callback) {
-
+  const env = Deno.env.toObject()
+  console.log(env)
   if (env.SESSION_TABLE_NAME === 'jwe')
     return jwe.read(request, callback)
 
