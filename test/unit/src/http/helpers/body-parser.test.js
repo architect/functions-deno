21ce7c4a798a79923da44c70218fa3b1fc8816b7
Martin Hicks <hello@martinhicks.net>
2021-04-28 19:29:59 +0100

    unit tests - helpers

---
+++ test/unit/src/http/helpers/body-parser.test.js
@@ -0,0 +1,125 @@
+import parseBody from '../../../../../src/http/helpers/body-parser.js'
+import { Buffer } from 'https://deno.land/std@0.93.0/node/buffer.ts'
+import {
+    assertThrows,
+    assertEquals,
+} from "https://deno.land/std@0.93.0/testing/asserts.ts"
+
+let str = i => JSON.stringify(i)
+let b64encode = i => Buffer.from(i).toString('base64')
+
+// Bodies
+let hi = { hi: 'there' }
+let hiBase64 = { base64: b64encode('hi there') } // Arc 5
+let hiBase64file = b64encode('hi there\n') // text file style
+let hiFormURL = b64encode('hi=there')
+
+// Content types
+let json = { 'Content-Type': 'application/json' }
+let formURLencoded = { 'Content-Type': 'application/x-www-form-urlencoded' }
+let multiPartFormData = { 'Content-Type': 'multipart/form-data' }
+let octetStream = { 'Content-Type': 'application/octet-stream' }
+
+Deno.test({
+  name: 'Architect v6+ requests', 
+  fn: () => {
+    //t.plan(9)
+    // HTTP + Lambda v2.0 payloads pass in raw JSON
+    let req = {
+      body: str(hi),
+      headers: json,
+      isBase64Encoded: false
+    }
+    assertEquals(str(parseBody(req)), str(hi), `body matches ${req.body}`)
+
+    // Pass through empty body (although in practice we'll never see this, as we transform to empty object)
+    req = {
+      body: null,
+      headers: json
+    }
+    assertEquals(str(parseBody(req)), str(null), `body matches ${str(req.body)}`)
+
+    req = {
+      body: b64encode(str(hi)),
+      headers: json,
+      isBase64Encoded: true
+    }
+    assertEquals(str(parseBody(req)), str(hi), `body matches ${str(req.body)}`)
+
+    // Alt JSON API
+    req = {
+      body: b64encode(str(hi)),
+      headers: { 'Content-Type': 'application/vnd.api+json' },
+      isBase64Encoded: true
+    }
+    assertEquals(str(parseBody(req)), str(hi), `body matches ${str(req.body)}`)
+
+    // Test faulty encoding on JSON posts
+    req.body = str(hi)
+    assertThrows(() => str(parseBody(req)), Error, 'Invalid request body encoding or invalid JSON')
+    req.body = b64encode('hello there')
+    assertThrows(() => str(parseBody(req)), Error, 'Invalid request body encoding or invalid JSON')
+
+    req = {
+      body: hiFormURL,
+      headers: formURLencoded,
+      isBase64Encoded: true
+    }
+    assertEquals(str(parseBody(req)), str(hi), `body matches ${str(req.body)}`)
+    // Not test faulty encoding on form URL-encoded posts; you'll always get something back
+
+    // Pass through multipart / base64
+    req = {
+      body: hiBase64file,
+      headers: multiPartFormData,
+      isBase64Encoded: true
+    }
+    assertEquals(str(parseBody(req)), str({ base64: hiBase64file }), `body matches ${str(req.body)}`)
+
+    // Pass through octet stream / base64
+    req.headers = octetStream
+    assertEquals(str(parseBody(req)), str({ base64: hiBase64file }), `body matches ${str(req.body)}`)
+  },
+  sanitizeResources: false,
+  sanitizeOps: false
+})
+
+Deno.test({
+    name: 'Architect v5 requests', 
+    fn: () => {
+    //t.plan(5)
+    // Pass through empty body
+    let req = {
+        body: {},
+        headers: json
+    }
+    assertEquals(parseBody(req), req.body, `body matches ${str(req.body)}`)
+
+    // Pass through parsed body (JSON)
+    req = {
+        body: hi,
+        headers: json
+    }
+    assertEquals(str(parseBody(req)), str(hi), `body matches ${str(req.body)}`)
+
+    // Pass through parsed body (formURLencoded)
+    req = {
+        body: hi,
+        headers: formURLencoded
+    }
+    assertEquals(str(parseBody(req)), str(hi), `body matches ${str(req.body)}`)
+
+    // Pass through multipart / base64
+    req = {
+        body: hiBase64,
+        headers: multiPartFormData
+    }
+    assertEquals(str(parseBody(req)), str(hiBase64), `body matches ${str(req.body)}`)
+
+    // Pass through octet stream / base64
+    req.headers = octetStream
+    assertEquals(str(parseBody(req)), str(hiBase64), `body matches ${str(req.body)}`)
+  },
+  sanitizeResources: false,
+  sanitizeOps: false
+})
---
+++ test/unit/src/http/helpers/url.test.js
@@ -0,0 +1,87 @@
+import url from '../../../../../src/http/helpers/url.js'
+import {
+    assert,
+    assertEquals,
+} from "https://deno.land/std@0.93.0/testing/asserts.ts"
+
+let env = Deno.env.get('NODE_ENV')
+
+function reset () {
+  Deno.env.delete('NODE_ENV')
+  Deno.env.delete('ARC_LOCAL')
+  if (Deno.env.get('NODE_ENV')) throw ReferenceError('NODE_ENV not unset')
+}
+
+Deno.test({
+  name: 'Set up env', 
+  fn: () => {
+    //t.plan(1)
+    assert(url, 'url helper found')
+  },
+  sanitizeResources: false,
+  sanitizeOps: false
+})
+
+Deno.test({
+  name: 'Local (NODE_ENV=testing) env returns unmodified URL', 
+  fn: () => {
+    //t.plan(1)
+    reset()
+    Deno.env.set('NODE_ENV', 'testing')
+    let asset = url('foo.png')
+    assertEquals(asset, 'foo.png', 'Returned unmodified path')
+  },
+  sanitizeResources: false,
+  sanitizeOps: false
+})
+
+Deno.test({
+  name: 'Staging env returns staging-prefixed URL', 
+  fn: () => {
+    //t.plan(1)
+    reset()
+    Deno.env.set('NODE_ENV', 'staging')
+    let asset = url('/')
+    assertEquals(asset, '/staging/', 'Returned staging path')
+  },
+  sanitizeResources: false,
+  sanitizeOps: false
+})
+
+Deno.test({
+  name: 'Local env with staging mask (NODE_ENV=staging, ARC_LOCAL=1) returns unmodified path', 
+  fn: () => {
+    //t.plan(1)
+    reset()
+    Deno.env.set('NODE_ENV', 'staging')
+    Deno.env.set('ARC_LOCAL', '1')
+    let asset = url('bar.png')
+    assertEquals(asset, 'bar.png', 'Returned staging path')
+  },
+  sanitizeResources: false,
+  sanitizeOps: false
+})
+
+Deno.test({
+  name: 'Production env returns production-prefixed URL', 
+  fn: () => {
+    //t.plan(1)
+    reset()
+    Deno.env.set('NODE_ENV', 'production')
+    let asset = url('/')
+    assertEquals(asset, '/production/', 'Returned staging path')
+  },
+  sanitizeResources: false,
+  sanitizeOps: false
+})
+
+Deno.test({
+  name: 'Reset', 
+  fn: () => {
+    reset()
+    Deno.env.set('NODE_ENV', env)
+    assertEquals(Deno.env.get('NODE_ENV'), env)
+  },
+  sanitizeResources: false,
+  sanitizeOps: false
+})
