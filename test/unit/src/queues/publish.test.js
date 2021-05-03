3651006aa96e8a7e4d6b3094ec728bc154cc6559
Martin Hicks <hello@martinhicks.net>
2021-05-03 17:21:41 +0100

    queues unit tests

---
+++ test/unit/src/queues/publish.test.js
@@ -0,0 +1,22 @@
+import publish from '../../../../src/queues/publish.js'
+import { assertThrows } from "https://deno.land/std@0.93.0/testing/asserts.ts"
+
+Deno.test({
+  name: 'queues.publish should throw if there is no parameter name', 
+  fn: () => {
+    //t.plan(1)
+    assertThrows(() => publish({}), ReferenceError, 'missing params.name', 'throws missing name parameter exception')
+  },
+  sanitizeResources: false,
+  sanitizeOps: false
+})
+
+Deno.test({
+  name: 'queues.publish should throw if there is no parameter payload', 
+  fn: () => {
+    //t.plan(1)
+    assertThrows(() => publish({ name: 'batman' }), ReferenceError, 'missing params.payload', 'throws missing payload parameter exception')
+  },
+  sanitizeResources: false,
+  sanitizeOps: false
+})
---
+++ test/unit/src/queues/subscribe.test.js
@@ -0,0 +1,57 @@
+import sinon from 'https://cdn.skypack.dev/sinon'
+import subscribe from '../../../../src/queues/subscribe.js'
+//import mockSqsEvent from '../../../mock/mock-sqs-event.json' -- https://github.com/denoland/deno/issues/7623
+const mockSqsEvent = JSON.parse(Deno.readTextFileSync('./test/mock/mock-sqs-event.json'));
+import {
+  assert,
+  AssertionError,
+  assertEquals,
+  assertExists,
+  assertNotEquals
+} from "https://deno.land/std@0.93.0/testing/asserts.ts"
+
+Deno.test({
+  name: 'queues.subscribe calls handler', 
+  fn: () => {
+    //t.plan(1)
+
+    let eventHandler = sinon.fake.yields()
+
+    // get a lambda signature from the handler
+    let handler = subscribe(eventHandler)
+
+    // invoke the lambda handler with mock payloads
+    let mockContext = {}
+    handler(mockSqsEvent, mockContext, function _handler (err) {
+      if (err) {
+        AssertionError(err)
+      }
+      else {
+        assert(eventHandler.calledOnce, 'event handler called once')
+      }
+    })
+  },
+  sanitizeResources: false,
+  sanitizeOps: false
+})
+
+Deno.test({
+  name: 'queues.subscribe calls async handler', 
+  fn: async () => {
+    //t.plan(1)
+
+    let fake = sinon.fake()
+
+    // get a lambda signature from the handler
+    // eslint-disable-next-line
+    let handler = subscribe(async function (json) {
+      fake(json)
+    })
+
+    // invoke the lambda handler with mock payloads
+    await handler(mockSqsEvent)
+    assert(fake.calledOnce, 'event handler called once')
+  },
+  sanitizeResources: false,
+  sanitizeOps: false
+})
