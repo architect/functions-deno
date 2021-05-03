4cfeb08e1b1c2cde4cf80d8b8b436afee395d242
Martin Hicks <hello@martinhicks.net>
2021-05-03 17:02:35 +0100

    jwe should be the default?

--- src/http/session/read.js
+++ src/http/session/read.js
@@ -3,7 +3,7 @@ import ddb from './providers/ddb/index.js'
 
 export default function read (request, callback) {
 
-  if (Deno.env.get('SESSION_TABLE_NAME') === 'jwe')
+  if (Deno.env.get('SESSION_TABLE_NAME') === 'jwe' || Deno.env.get('SESSION_TABLE_NAME') === undefined)
     return jwe.read(request, callback)
 
   return ddb.read(request, callback)
